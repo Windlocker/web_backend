63799648fa95b148d0baaa90c1b2ab67b8a71de9
iwin2471 <iwin12372@gmail.com>
2017-07-18 14:36:59 +0900

    index redirect remove

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 module.exports = (router)=>{
   router.get('/', function(req, res, next) {
-    if(req.session.name) return res.redirect("http://iwin247.kr:3003/menu");
+    if(req.session.name) res.redirect("http://iwin247.kr:3003/menu");
     res.render('index', { title: 'Express' });
   })
 
