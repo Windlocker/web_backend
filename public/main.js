4b6c9d5ac4bf05bbf1b5503d003844436eb65881
iwin2471 <iwin12372@gmail.com>
2017-07-18 17:54:16 +0900

    fornt update

--- public/main.js
+++ public/main.js
@@ -133,3 +133,7 @@ if($(this).val().length >= 5)
 
 });
 $(document).ready(function(){ var fileTarget = $('.filebox .upload-hidden'); fileTarget.on('change', function(){ if(window.FileReader){ var filename = $(this)[0].files[0].name; } else {  var filename = $(this).val().split('/').pop().split('\\').pop();  } $(this).siblings('.upload-name').val(filename); }); });
+$(".button-collapse").sideNav();
+$(document).ready(function() {
+   $('.rip').flexVerticalCenter();
+ });
